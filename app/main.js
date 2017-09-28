b491c4e05ca9c460dfdb70c08d0f2436ddd9bf18
Clark Murray <clarkamurray@gmail.com>
2017-09-28 14:20:41 -0400

    MVP except for win on turn 9

--- app/main.js
+++ app/main.js
@@ -3,6 +3,8 @@ var restartButton = document.getElementById("restartButton");
 var spaces = document.getElementsByClassName('space');
 var symbols = ['O', 'X'];
 var turn = 0;
+var notification = document.getElementById('notification');
+var winnerMessage = document.getElementById('winnerMessage');
 
 
 
@@ -20,7 +22,10 @@ function startGame() {
 	// Reset turn counter
 	turn = 0;
 
-	// Remove winner notification
+	// Remove winner notification and make sure div is hidden
+	notification.style.display = 'none';
+	winnerMessage.innerHTML = '';
+
 
 	// Clear board and add click events on squares
 	for (i = 0; i < spaces.length; i++ ) {
@@ -32,23 +37,44 @@ function startGame() {
 function takeSpace() {
 
 	turn++;
-	this.innerHTML = symbols[turn % 2];
+	var currentPlayer = symbols[turn % 2];
+	this.innerHTML = currentPlayer;
 	this.removeEventListener("click", takeSpace);
 
 	for (i = 0; i < wins.length; i++) {
-		if (checkForWin(wins[i])) {
-			alert("Whoo hoo! Someone won!");
+		if ( checkForWin( wins[ i ] ) ) {
+			
+			// No more clicking!
+			for (j = 0; j < spaces.length; j++ ) {
+				spaces[j].removeEventListener("click", takeSpace);
+			}
+
+			// Notify the players 
+			notification.style.display = 'block';
+			winnerMessage.innerHTML = "Yay! " + currentPlayer + " won!";
+
+
+		} else {
+
+			if (turn === 9) {
+				notification.style.display = "block";
+				winnerMessage.innerHTML = "CAT";
+			}
 		}
 	}
+
 }
 
 function checkForWin(winArray){
 
 	// Wins happens if all these indices contain the same symbol
 
-	return spaces[winArray[0]] !== '' && 
-	spaces[winArray[0]] === spaces[winArray[1]] &&
-	spaces[winArray[0]] === spaces[winArray[2]];
+	let result = spaces[winArray[0]].innerHTML !== '' && 
+	spaces[winArray[0]].innerHTML === spaces[winArray[1]].innerHTML &&
+	spaces[winArray[0]].innerHTML === spaces[winArray[2]].innerHTML;
+
+	console.log( 'checkForWin ', result );
+	return result;
 
 
 }
